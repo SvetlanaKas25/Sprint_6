1880de61a408951a2e37b4f02cd18bb29134bcd8
Svetlana Kasianova <SvetikSVDN@yandex.ru>
2026-05-25 12:47:07 +0300

    Создан тест Проверка ответов на вопросы в test_main_page_FAQ.py

---
+++ tests/test_main_page_FAQ.py
@@ -0,0 +1,21 @@
+import allure
+import pytest
+
+from data import AnswerText, QuestionText
+from pages.main_page import MainPage
+
+
+class TestMainPage:
+
+    @allure.title("Проверка ответов на вопросы")
+    @allure.description("Проверяем, что при нажиматии на вопрос, открывается соответствующий текст ответа.")
+    @pytest.mark.parametrize("index", range(len(QuestionText.QUESTIONS)))
+    def test_questions_and_answers(self, driver, index):
+        main_page = MainPage(driver)
+       
+        expected_answer_text = AnswerText.ANSWERS[index]
+        actual_answer_text = main_page.get_answer_text(index)
+        
+        assert actual_answer_text == expected_answer_text, (
+                f'Ожидали ответ {expected_answer_text}, '
+                f'получили {actual_answer_text}')
