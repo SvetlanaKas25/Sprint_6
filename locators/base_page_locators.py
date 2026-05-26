78317623ed8963e499403b473e1193abb695b553
Svetlana Kasianova <SvetikSVDN@yandex.ru>
2026-05-26 14:33:28 +0300

    Локаторы для order_page.py удалены из base_page_locators.py

--- locators/base_page_locators.py
+++ locators/base_page_locators.py
@@ -5,12 +5,6 @@ class BasePageLocators:
    # Кнопка Принять куки
    COOKIE_BUTTON = (By.ID, "rcc-confirm-button") 
 
-   # Кнопка Заказать вверху главной страницы
-   ORDER_BUTTON_HEADER = (By.XPATH, ".//div[@class='Header_Nav__AGCXC']/button[@class='Button_Button__ra12g']")
-
-   # Кнопка Заказать внизу главной страницы
-   ORDER_BUTTON_MIDDLE = (By.XPATH, "//div[@class='Home_FinishButton__1_cWm']/button[contains(text(), 'Заказать')]")
-
    # Логотип Яндекс
    LOGO_YANDEX = (By.CLASS_NAME, "Header_LogoYandex__3TSOI") 
 
