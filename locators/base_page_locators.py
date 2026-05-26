99daa7d38f9d28f4885c2e886600722a58389ce8
Svetlana Kasianova <SvetikSVDN@yandex.ru>
2026-05-26 14:39:10 +0300

    Исправлено: локаторы для header_menu.py сохранены в header_menu_locators.py

--- locators/base_page_locators.py
+++ locators/base_page_locators.py
@@ -5,8 +5,4 @@ class BasePageLocators:
    # Кнопка Принять куки
    COOKIE_BUTTON = (By.ID, "rcc-confirm-button") 
 
-   # Логотип Яндекс
-   LOGO_YANDEX = (By.CLASS_NAME, "Header_LogoYandex__3TSOI") 
-
-   # Логотип Самокат
-   LOGO_SCOOTER = (By.CLASS_NAME, "Header_LogoScooter__3lsAR") 
+   
---
+++ locators/header_menu_locators.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+
+class HeaderMenuLocators:
+    # Логотип Яндекс
+    LOGO_YANDEX = (By.CLASS_NAME, "Header_LogoYandex__3TSOI") 
+
+    # Логотип Самокат
+    LOGO_SCOOTER = (By.CLASS_NAME, "Header_LogoScooter__3lsAR") 
