ea3f0ad1093fb7bf6a58544158ae367c0da7ff2d
Svetlana Kasianova <SvetikSVDN@yandex.ru>
2026-05-26 13:41:08 +0300

    Добавлены декораторы allure.step перед методами в main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,13 +1,14 @@
+import allure
 from pages.base_page import BasePage
 from locators.main_page_locators import MainPageLocators
 
 class MainPage(BasePage):
     
-    #'Кликаем на вопрос номер {number}'
+    @allure.step("Кликаем на вопрос номер {number}")
     def click_question(self, number):
         self.click_element(MainPageLocators.QUESTION_LOCATORS(number))
 
-    #'Получаем текст ответа на вопрос {number}'
+    @allure.step("Получаем текст ответа на вопрос {number}")
     def get_answer_text(self, number):
         self.accept_cookies()
         self.scroll_page_down()
